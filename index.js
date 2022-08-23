b58e20e70190a5b27d3ee65abfad43941e5028bf
moringawonder <adams.wonderboy@student.moringaschool.com>
2022-08-23 19:50:02 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,28 @@
+// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    }
+});
+
+// rightDodger
+function moveDodgerRight() {
+    let left = dodger.style.left.replace("px", "")
+    left = parseInt(left)
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
